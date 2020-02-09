fecb509b5a58222e40e83c2f03d53d050f101445
filipebotti <filipebotti@hotmail.com>
2020-02-09 19:02:29 -0300

    Clean App.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+export default App;
