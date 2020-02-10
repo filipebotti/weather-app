fdfee836bfae3d684b381eea6a716456c9019ab5
filipebotti <filipebotti@hotmail.com>
2020-02-09 23:16:19 -0300

    Shadowed Text

---
+++ src/components/UI/ShadowText/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+import Text from '../Text'
+
+export default styled(Text).attrs(props => ({
+  shadowOffset: props.shadowOffset || { width: 0, height: 3}
+}))`
+  shadowColor: ${props => props.shadowColor || 'black'}
+  shadowOpacity: ${props => props.shadowOpacity || 0.16}
+  shadowRadius: ${props => props.shadowRadius || 6 }
+`
