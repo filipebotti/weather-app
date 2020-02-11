c619da8ddd4f58d60efcba9e54c8098d74ffe3f4
filipebotti <filipebotti@hotmail.com>
2020-02-11 00:35:35 -0300

    WeatherPage View Component

--- src/pages/Weather/index.js
+++ src/pages/Weather/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { Text } from '../../components/UI'
 import { ForecastCard, ForecastInfo } from '../../components/Weather'
 
 const Wrapper = styled.View`
@@ -8,17 +9,46 @@ const Wrapper = styled.View`
 `
 const WeatherWrapper = styled.View`
   flex: 1
-  backgroundColor: ${props => props.theme.colors.primaryColor}
+  backgroundColor: ${props => props.theme.colors.primaryColor}  
+  alignItems: center
+  paddingTop: 15
+  paddingBottom: 95.5
+  justifyContent: space-between
+`
+const CityText = styled(Text)`
+  color: ${props => props.theme.colors.backgroundColor}
+  fontSize: 20
+  lineHeight: 24
 `
 
-const WeatherPage = ({ temp }) => {
+const WeatherPage = (props) => {
+
+  const {
+    temp,
+    pressure,
+    winds,
+    windsDirection,
+    humidity,
+    min,
+    max
+  } = props
+
   return (
     <Wrapper>
       <WeatherWrapper>
+        <CityText>Fortaleza, CE</CityText>
         <ForecastCard
           temp={temp}
         />
       </WeatherWrapper>
+      <ForecastInfo
+        pressure={pressure}
+        winds={winds}
+        windsDirection={windsDirection}
+        humidity={humidity}
+        min={min}
+        max={max}      
+      />
     </Wrapper>
   )
 }
